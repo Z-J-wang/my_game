9cc3d1274c14af4b24d52062470e77bcddb88e3f
Jay <1356573671@qq.com>
2020-06-27 17:59:38 +0800

    feat: 贪吃蛇搭建完成

--- src/components/Gluttonous_Snake/index.vue
+++ src/components/Gluttonous_Snake/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="hello">
+    <div id="Gluttonous_Snake">
         <div class="background">
             <p class="score">
                 您的成绩：<span>{{ score }}</span>
@@ -21,6 +21,7 @@
                         type="button"
                         id="btn_begin"
                         value="开始"
+                        @click="beginGame()"
                     /></li>
                 <li>
                     <select v-model="diffculty">
@@ -54,6 +55,8 @@
 <script lang="ts">
 // import Vue from "vue";
 import { Component, Vue, Watch } from "vue-property-decorator";
+import Snake from "./snake";
+declare var $: any;
 
 @Component
 export default class Gluttonous_Snake extends Vue {
@@ -61,11 +64,56 @@ export default class Gluttonous_Snake extends Vue {
     private showMenu: boolean = true; // 菜单显示控制
     private diffculty: string = ""; // 游戏难度
     private score: number = 0; // 游戏得分
+    private snake!: Snake;
 
-    created() {
+    mounted() {
+        let $background = $(".background");
         this.diffculty = "简单";
+        this.snake = new Snake($background, this.score, 500);
+        $(window).keydown((event: any) => {
+            //为窗口添加键盘事件，
+            switch (event.keyCode) {
+                case 38:
+                    {
+                        //判断单击按键是否为上
+                        this.snake.move_top(); //为真，则向上移动
+                        this.snake.eat(); //执行eat()函数，判断移动的新位置是否有食物
+                        if (this.snake.status != "Bottom") {
+                            //判断将要移动的方向是否与现在的移动方向相反，相反则status不变
+                            this.snake.status = "Top";
+                        }
+                    }
+                    break;
+                case 40:
+                    {
+                        this.snake.move_bottom();
+                        this.snake.eat();
+                        if (this.snake.status != "Top") {
+                            this.snake.status = "Bottom";
+                        }
+                    }
+                    break;
+                case 37:
+                    {
+                        this.snake.move_left();
+                        this.snake.eat();
+                        if (this.snake.status != "Right") {
+                            this.snake.status = "Left";
+                        }
+                    }
+                    break;
+                case 39:
+                    {
+                        this.snake.move_right();
+                        this.snake.eat();
+                        if (this.snake.status != "Left") {
+                            this.snake.status = "Right";
+                        }
+                    }
+                    break;
+            }
+        });
     }
-
     /**
      * 监听游戏难度切换操作
      */
@@ -79,175 +127,184 @@ export default class Gluttonous_Snake extends Vue {
 
         switch (val) {
             case "简单":
-                console.log(degreeOfDifficulty.简单);
+                this.snake.difficult = 500;
                 break;
             case "困难":
-                console.log(degreeOfDifficulty.困难);
+                this.snake.difficult = 200;
                 break;
             case "地狱":
-                console.log(degreeOfDifficulty.地狱);
+                this.snake.difficult = 50;
                 break;
         }
+    }
+
+    private beginGame(): void {
+        this.showMenu = false;
+        this.showSnake = true;
+        this.snake.startGame();
     }
 
     /**
      * 退出游戏
      */
-    private exit():void{
-        super.$router.push({path:'/'})
+    private exit(): void {
+        super.$router.push({ path: "/" });
     }
 
     /**
      * 重新开始游戏
      */
-    private restart():void{
+    private restart(): void {
         window.location.reload();
     }
 }
 </script>
 
-<style scoped>
-.hello {
+<style lang="less">
+#Gluttonous_Snake {
     position: relative;
-}
-.background {
-    position: relative;
-    z-index: -1;
-    margin: 20px auto;
-    width: 1000px;
-    height: 600px;
-    /*line-height: 600px;*/
-    border: solid 2px;
-    background-color: #8cc63e;
-}
-.snake {
-    display: inline-block;
-    border: solid 1px;
-    width: 20px;
-    height: 20px;
-    background-color: gray;
-    position: absolute;
-    z-index: 10px;
-}
-.start {
-    top: 300px;
-    left: 100px;
-}
-.snake_boby {
-    display: inline-block;
-    border: solid 1px;
-    width: 20px;
-    height: 20px;
-    background-color: #7dbb32;
-    position: absolute;
-    border-radius: 40%;
-    z-index: 10px;
-}
-.foot {
-    display: inline-block;
-    width: 20px;
-    height: 20px;
-    background: url("../../assets/image/Gluttonous_Snake/food.png") no-repeat;
-    background-size: 20px;
-    position: absolute;
-    border-radius: 40%;
-    z-index: 10px;
-}
-.begin,
-.GameOver {
-    position: absolute;
-    border: solid 1px;
-    background: url("../../assets/image/Gluttonous_Snake/贪吃蛇.jpg") no-repeat;
-    background-size: 600px;
-    width: 600px;
-    height: 450px;
-    top: 60px;
-    left: 50%;
-    transform: translateX(-50%);
-}
-.begin ul {
-    position: absolute;
-    top: 200px;
-    left: 420px;
-}
-.begin ul li {
-    margin-bottom: 20px;
-    /*background: red;*/
-}
-.begin ul li input {
-    width: 150px;
-    height: 50px;
-    line-height: 30px;
-    border: none;
-    background-color: #7dbb32;
-    cursor: pointer;
-    border-radius: 0.3em;
-    box-shadow: 0 1px white inset;
-    text-align: center;
-    text-shadow: 0 1px 1px black;
-    color: white;
-    font-weight: bold;
-    outline: none;
-}
-.begin ul li input:hover,
-.begin ul li select:hover,
-.GameOver input:hover {
-    background: #a3d35b;
-}
-.begin ul li input:active,
-.begin ul li select:active,
-.GameOver input:active {
-    box-shadow: 0.05em 0.1em 0.2em rgba(0, 0, 0, 0.6) inset;
-}
-.begin ul li select {
-    width: 150px;
-    height: 50px;
-    line-height: 30px;
-    /*padding-left: 55px;*/
-    text-indent: 55px;
-    background-color: #7dbb32;
-    border-radius: 0.3em;
-    box-shadow: 0 1px white inset;
-    text-align: center;
-    text-shadow: 0 1px 1px black;
-    color: white;
-    font-weight: bold;
-    outline: none;
-}
+    .background {
+        position: relative;
+        z-index: -1;
+        margin: 20px auto;
+        width: 1000px;
+        height: 600px;
+        /*line-height: 600px;*/
+        border: solid 2px;
+        background-color: #8cc63e;
+    }
+    .snake {
+        display: inline-block;
+        border: solid 1px;
+        width: 20px;
+        height: 20px;
+        background-color: gray;
+        position: absolute;
+        z-index: 10px;
+    }
+    .start {
+        top: 300px;
+        left: 100px;
+    }
+    .snake_boby {
+        display: inline-block;
+        border: solid 1px;
+        width: 20px;
+        height: 20px;
+        background-color: #7dbb32;
+        position: absolute;
+        border-radius: 40%;
+        z-index: 10px;
+    }
+    .foot {
+        position: absolute;
+        display: inline-block;
+        width: 20px;
+        height: 20px;
+        background: url("../../assets/image/Gluttonous_Snake/food.png")
+            no-repeat;
+        background-size: 20px;
+        position: absolute;
+        border-radius: 40%;
+        z-index: 10px;
+    }
+    .begin,
+    .GameOver {
+        position: absolute;
+        border: solid 1px;
+        background: url("../../assets/image/Gluttonous_Snake/贪吃蛇.jpg")
+            no-repeat;
+        background-size: 600px;
+        width: 600px;
+        height: 450px;
+        top: 60px;
+        left: 50%;
+        transform: translateX(-50%);
+    }
+    .begin ul {
+        position: absolute;
+        top: 200px;
+        left: 420px;
+    }
+    .begin ul li {
+        margin-bottom: 20px;
+        /*background: red;*/
+    }
+    .begin ul li input {
+        width: 150px;
+        height: 50px;
+        line-height: 30px;
+        border: none;
+        background-color: #7dbb32;
+        cursor: pointer;
+        border-radius: 0.3em;
+        box-shadow: 0 1px white inset;
+        text-align: center;
+        text-shadow: 0 1px 1px black;
+        color: white;
+        font-weight: bold;
+        outline: none;
+    }
+    .begin ul li input:hover,
+    .begin ul li select:hover,
+    .GameOver input:hover {
+        background: #a3d35b;
+    }
+    .begin ul li input:active,
+    .begin ul li select:active,
+    .GameOver input:active {
+        box-shadow: 0.05em 0.1em 0.2em rgba(0, 0, 0, 0.6) inset;
+    }
+    .begin ul li select {
+        width: 150px;
+        height: 50px;
+        line-height: 30px;
+        /*padding-left: 55px;*/
+        text-indent: 55px;
+        background-color: #7dbb32;
+        border-radius: 0.3em;
+        box-shadow: 0 1px white inset;
+        text-align: center;
+        text-shadow: 0 1px 1px black;
+        color: white;
+        font-weight: bold;
+        outline: none;
+    }
 
-.GameOver {
-    display: none;
-}
-.GameOver input {
-    width: 180px;
-    height: 60px;
-    line-height: 60px;
-    border: none;
-    /*background-color: #7dbb32;*/
-    background: transparent;
-    cursor: pointer;
-    border-radius: 0.3em;
-    box-shadow: 0 1px white inset;
-    text-align: center;
-    text-shadow: 0 1px 1px black;
-    color: white;
-    font-size: 20px;
-    font-weight: bold;
-    outline: none;
-    position: absolute;
-    top: 220px;
-    left: 400px;
-}
-.score {
-    position: absolute;
-    font-size: 20px;
-    top: -30px;
-    left: 0px;
-}
-.grade {
-    position: absolute;
-    font-size: 20px;
-    top: -30px;
-    left: 800px;
+    .GameOver {
+        display: none;
+    }
+    .GameOver input {
+        width: 180px;
+        height: 60px;
+        line-height: 60px;
+        border: none;
+        /*background-color: #7dbb32;*/
+        background: transparent;
+        cursor: pointer;
+        border-radius: 0.3em;
+        box-shadow: 0 1px white inset;
+        text-align: center;
+        text-shadow: 0 1px 1px black;
+        color: white;
+        font-size: 20px;
+        font-weight: bold;
+        outline: none;
+        position: absolute;
+        top: 220px;
+        left: 400px;
+    }
+    .score {
+        position: absolute;
+        font-size: 20px;
+        top: -30px;
+        left: 0px;
+    }
+    .grade {
+        position: absolute;
+        font-size: 20px;
+        top: -30px;
+        left: 800px;
+    }
 }
 </style>
